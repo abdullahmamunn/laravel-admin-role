9de04b786f46a060749fb01d93ff262f80cb4726
Abdullah Al Mamun <almamun00021@gmail.com>
2021-07-09 19:28:08 +0600

    tamplate mastering

---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield('title', 'Admin Role Management')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('backend.layouts.partials.styles')
+</head>
+<body>
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+      @include('backend.layouts.partials.sidebar')
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+           @include('backend.layouts.partials.header')
+            <!-- header area end -->
+          @yield('content')
+        </div>
+        <!-- main content area end -->
+       @include('backend.layouts.partials.footer')
+    </div>
+    <!-- page container area end -->
+   @include('backend.layouts.partials.scripts')
+</body>
+
+</html>
